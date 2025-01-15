438e55743c5d291b4609dac1df730e5c1362a3de
acidicneko <ayush7788899@gmail.com>
2025-01-15 21:49:23 +0530

    feat: compile multiple files parallelly

--- src/build.cpp
+++ src/build.cpp
@@ -4,7 +4,10 @@
 #include <dirent.h>
 #include <iostream>
 #include <meta.hpp>
+#include <mutex>
+#include <queue>
 #include <string>
+#include <thread>
 #include <utils.hpp>
 #include <vector>
 
@@ -42,22 +45,50 @@ int build::Build() {
   std::vector<std::string> SourceFiles = CollectSourceFiles("src", 1);
   std::vector<std::string> ObjectFiles;
   bool CompiledSuccessfully = true;
-  for (auto file = SourceFiles.begin(); file != SourceFiles.end(); file++) {
-    std::string SrcFileLocation = file->data();
 
-    if (dependency::CheckModified(SrcFileLocation)) {
-      if (Compiler->Compile(SrcFileLocation) != 0) {
-        CompiledSuccessfully = false;
-        std::cout << "\033[1;31mERROR\033[0m: Failed to compile: "
-                  << file->data() << std::endl;
-        continue;
-      }
-    }
-    std::string ObjFileLocation = SrcFileLocation.replace(0, 3, "build");
-
-    ObjectFiles.push_back(
-        SrcFileLocation.replace(ObjFileLocation.find(".", 0, 1) + 1, 3, "o"));
+  std::queue<std::string> SourceFilesQueue;
+  std::mutex SourceFilesQueueMutex;
+  for (const auto &file : SourceFiles) {
+    SourceFilesQueue.push(file);
   }
+
+  auto worker = [&]() {
+    while (true) {
+      std::string file;
+      {
+        std::lock_guard<std::mutex> lock(SourceFilesQueueMutex);
+        if (SourceFilesQueue.empty()) {
+          break;
+        }
+        file = SourceFilesQueue.front();
+        SourceFilesQueue.pop();
+      }
+      if (dependency::CheckModified(file)) {
+        if (Compiler->Compile(file) != 0) {
+          CompiledSuccessfully = false;
+          std::cout << "\033[1;31mERROR\033[0m: Failed to compile: " << file
+                    << std::endl;
+          continue;
+        }
+      }
+      std::string ObjFileLocation = file.replace(0, 3, "build");
+
+      ObjectFiles.push_back(
+          file.replace(ObjFileLocation.find(".", 0, 1) + 1, 3, "o"));
+    }
+  };
+
+  std::vector<std::thread> threads;
+  for (int i = 0; i < GetThreads(); i++) {
+    threads.emplace_back(std::thread(worker));
+  }
+
+  for (auto &thread : threads) {
+    if (thread.joinable()) {
+      thread.join();
+    }
+  }
+
   if (CompiledSuccessfully) {
     Compiler->Link(ObjectFiles);
   } else {
--- src/include/meta.hpp
+++ src/include/meta.hpp
@@ -1,4 +1,15 @@
 #pragma once
 
 #define VERSION "1.0.0"
-#define DEFAULT_META_FILE ".scbuildinfo"
+#define DEFAULT_THREAD_COUNT 4
+
+#include <stddef.h>
+#include <thread>
+
+static inline size_t GetThreads() {
+  size_t t = std::thread::hardware_concurrency();
+  if (t == 0) {
+    return DEFAULT_THREAD_COUNT;
+  }
+  return t;
+}
